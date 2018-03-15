87b3450c0bdaf3754d84dc19e751f663756a6e5c
kangren <909296146@qq.com>
2018-03-15 16:25:55 +0800

    评论显示页面基本完成

---
+++ app/src/main/java/com/kang/commentview/CommentView.java
@@ -0,0 +1,277 @@
+package com.kang.commentview;
+
+import java.util.List;
+
+import com.orhanobut.logger.Logger;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Scroller;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * 仿今日头条可下拉关闭弹窗评论
+ * Created by kangren on 2018/3/15.
+ */
+
+public class CommentView extends LinearLayout
+{
+
+    /**
+     * 下拉复位距离，超过这个距离PopupWindow窗口关闭 单位dp
+     */
+    private final static int RESET_DISTANCE = 80;
+
+    @BindView(R.id.comment_close)
+    ImageView closeImg;
+
+    @BindView(R.id.comment_num)
+    TextView numText;
+
+    @BindView(R.id.comment_list)
+    ListView listView;
+
+    private Context mContext;
+
+    private List<String> lists;
+
+    private OnClickListener onClickListener;
+
+    /**
+     * 用于完成回滚操作的实例
+     */
+    private Scroller mScroller;
+
+    /**
+     * ACTION_DOWN的Y坐标
+     */
+    private float mDownY;
+
+    /**
+     * 当前ACTION_MOVE的Y坐标
+     */
+    private float mMoveY;
+
+    /**
+     * 上一ACTION_MOVE的Y坐标
+     */
+    private float mLastMoveY;
+
+    private int resetDistance;
+
+    public CommentView(Context context, List<String> list)
+    {
+        super(context);
+        mContext = context;
+        lists = list;
+        init();
+    }
+
+    private void init()
+    {
+        resetDistance = (int) (RESET_DISTANCE * mContext.getResources().getDisplayMetrics().density);
+        mScroller = new Scroller(mContext);
+        inflate(mContext, R.layout.comment_view, this);
+        ButterKnife.bind(this);
+
+        listView.setAdapter(new CommentAdapter());
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                if (onClickListener != null)
+                {
+                    onClickListener.onItemClick(parent, view, position, id);
+                }
+            }
+        });
+    }
+
+    @OnClick(R.id.comment_close)
+    public void onViewClicked(View view)
+    {
+        if (onClickListener != null)
+        {
+            onClickListener.onCloseClick(view);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev)
+    {
+        if (isFirstItemVisible())
+        {
+            switch (ev.getAction())
+            {
+                case MotionEvent.ACTION_DOWN:
+                    mDownY = ev.getRawY();
+                    mLastMoveY = mDownY;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    mMoveY = ev.getRawY();
+                    float diff = mMoveY - mLastMoveY;
+                    mLastMoveY = mMoveY;
+                    if (diff > 0)
+                    {
+                        Logger.d("true");
+                        return true;
+                    }
+                    break;
+            }
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_MOVE:
+                mMoveY = event.getRawY();
+                int scrolledY = (int) (mLastMoveY - mMoveY);
+                scrollBy(0, scrolledY);
+                mLastMoveY = mMoveY;
+                break;
+            case MotionEvent.ACTION_UP:
+                if (getScrollY() < -resetDistance)
+                {
+                    if (onClickListener != null)
+                    {
+                        onClickListener.onCloseClick(null);
+                        //窗口关闭后复位，不然下次弹出可能会保留上次滑动的位置
+                        scrollTo(0, 0);
+                    }
+                }
+                else
+                {
+                    //弹性复位
+                    mScroller.startScroll(0, getTop() + getScrollY(), 0, -getScrollY(), 500);
+                    invalidate();
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void computeScroll()
+    {
+        //重写computeScroll()方法，并在其内部完成平滑滚动的逻辑
+        if (mScroller.computeScrollOffset())
+        {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            invalidate();
+        }
+    }
+
+    /**
+     * 判断第一个child是否完全显示出来,即是否触顶
+     * 
+     * @return true完全显示出来，否则false
+     */
+    private boolean isFirstItemVisible()
+    {
+        final Adapter adapter = listView.getAdapter();
+
+        if (null == adapter || adapter.isEmpty())
+        {
+            return true;
+        }
+        // 第一个可见item在ListView中的位置
+        if (listView.getFirstVisiblePosition() == 0)
+        {
+            // getChildCount是当前屏幕可见范围内的count
+            int mostTop = (listView.getChildCount() > 0) ? listView.getChildAt(0).getTop() : 0;
+            if (mostTop >= 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 点击事件接口抛出
+     * @param listener listener
+     */
+    public void setOnClickListener(OnClickListener listener)
+    {
+        onClickListener = listener;
+    }
+
+    public interface OnClickListener
+    {
+        void onCloseClick(View view);
+
+        void onItemClick(AdapterView<?> parent, View view, int position, long id);
+    }
+
+    private class CommentAdapter extends BaseAdapter
+    {
+
+        public CommentAdapter()
+        {
+            super();
+        }
+
+        @Override
+        public int getCount()
+        {
+            return lists.size();
+        }
+
+        @Override
+        public Object getItem(int position)
+        {
+            return lists.get(position);
+        }
+
+        @Override
+        public long getItemId(int position)
+        {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            ViewHolder viewHolder = null;
+            if (convertView == null)
+            {
+                convertView = View.inflate(mContext, R.layout.comment_item, null);
+                viewHolder = new ViewHolder(convertView);
+                convertView.setTag(viewHolder);
+            }
+            else
+            {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            viewHolder.textView.setText(lists.get(position));
+            return convertView;
+        }
+    }
+
+    private class ViewHolder
+    {
+        TextView textView;
+
+        public ViewHolder(View itemView)
+        {
+            textView = itemView.findViewById(R.id.item_text);
+        }
+    }
+}
--- app/src/main/java/com/kang/commentview/MainActivity.java
+++ app/src/main/java/com/kang/commentview/MainActivity.java
@@ -1,15 +1,23 @@
 package com.kang.commentview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,7 +38,7 @@ public class MainActivity extends AppCompatActivity
     @BindView(R.id.comment_container)
     FrameLayout commentContainer;
 
-    private CommentPopup commentPopup;
+    private PopupWindow popupWindow;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -43,12 +51,68 @@ public class MainActivity extends AppCompatActivity
         webView.loadUrl("http://www.baidu.com");
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
+
+        List<String> lists = new ArrayList<>();
+        lists.add("第一个");
+        lists.add("第2个");
+        lists.add("第3个");
+        lists.add("第4个");
+        lists.add("第5个");
+        lists.add("第6个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+        lists.add("第7个");
+
+        CommentView view = new CommentView(this, lists);
+        view.setOnClickListener(new CommentView.OnClickListener() {
+            @Override
+            public void onCloseClick(View view) {
+                if (popupWindow != null) {
+                    popupWindow.dismiss();
+                }
+            }
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Click " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        popupWindow = new PopupWindow(view);
+        //设置SelectPicPopupWindow弹出窗体的宽
+        popupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+
+        //设置SelectPicPopupWindow弹出窗体的高
+        popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        //设置SelectPicPopupWindow弹出窗体可点击
+        popupWindow.setFocusable(true);
+
+        //设置SelectPicPopupWindow弹出窗体动画效果
+        popupWindow.setAnimationStyle(R.style.popwin_anim_style);
+
+        //实例化一个ColorDrawable颜色为透明
+        ColorDrawable dw = new ColorDrawable(0x00FFFFFF);
+
+        //设置SelectPicPopupWindow弹出窗体的背景
+        popupWindow.setBackgroundDrawable(dw);
     }
 
     @OnClick(R.id.send_comment)
     public void onViewClicked() {
-//        commentPopup = new CommentPopup(this);
-//        commentPopup.showAtLocation(commentContainer, Gravity.BOTTOM, 0, 0);
-        startActivity(new Intent(this, ScrollActivity.class));
+        popupWindow.showAtLocation(commentContainer, Gravity.BOTTOM, 0, 0);
     }
 }
